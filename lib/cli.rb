0ac84ecf8cae829014f4161b9814739357adf0ac
tateejt <tateejt@gmail.com>
2019-11-05 20:23:20 -0700

    Update cli.rb

--- lib/cli.rb
+++ lib/cli.rb
@@ -8,7 +8,7 @@ class CLI
         Scraper.get_articles
         print_stories
         puts " "
-        puts "Select an article you would like to see more information on or type list or type 'exit' in order to exit"
+        puts "Select an article you would like to see more information on or type 'list' or type 'exit' in order to exit"
         
         menu
         
@@ -18,7 +18,7 @@ class CLI
     def menu
         input = gets.strip.downcase
         while input != 'exit' do 
-            validNumber = 1..5
+            validNumber = [1, 2, 3, 4, 5]
             if validNumber 
                 article = Article.all[input.to_i - 1]
                 Scraper.get_article_details(article) if !article.story
@@ -30,7 +30,7 @@ class CLI
             end
             puts " "
             puts "Would you like to read another article?"
-            puts "If so, Select an article you would like to see more information on or type list or type 'exit' in order to exit"
+            puts "If so, Select an article you would like to see more information on or type 'list' or type 'exit' in order to exit"
             input = gets.strip.downcase
         end
         puts "Goodbye!"
